00c2ec68ab77f36106073f2998c17a9316521099
JohnJohnsonTaren <tytar.den@gmail.com>
2025-04-13 20:27:14 +0300

    https://github.com/JohnJohnsonTaren/Module1-Dev.git

---
+++ src/test/java/MyName/MainTest.java
@@ -0,0 +1,12 @@
+package MyName;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
